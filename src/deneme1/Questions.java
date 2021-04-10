b9040e94aeedc70e60fe3c50177daf93148ad890
nsolak <nurcannsolak@gmail.com>
2021-04-10 16:07:21 +0300

    commit

--- src/deneme1/Questions.java
+++ src/deneme1/Questions.java
@@ -4,7 +4,7 @@ public class Questions {
 
 	public static void main(String[] args) {
 	System.out.println("Nurcan'dan merkeze");
-        System.out.println("Nurcandan merkeze 2");
+    System.out.println("Nurcandan merkeze 2");
     
 	}
 
